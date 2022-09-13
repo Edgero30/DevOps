7724d4205dd86e508dd496a3c003932455499427
khldoon <218110351@psu.edu.sa>
2022-09-13 09:37:26 +0300

    changed s in system to capital

---
+++ demo.java
@@ -0,0 +1,8 @@
+public class Demo{
+    public static void main(string[] args){
+        System.out.println("kkkkkk");
+        System.out.println("hi");
+        System.out.println("lol");
+        System.out.println("gg");
+    }
+}
