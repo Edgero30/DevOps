5fbaf9586ffdd503b84132869ad5440d5dce7168
khldoon <218110351@psu.edu.sa>
2022-09-14 08:48:31 +0300

    sout added

--- demo.java
+++ demo.java
@@ -1,8 +1,12 @@
-public class Demo{
-    public static void main(string[] args){
+ public class Demo{
+    public static void main(String[] args){
         System.out.println("kkkkkk");
         System.out.println("hi");
         System.out.println("lol");
         System.out.println("gg");
+        System.out.println("hahaha");
+        System.out.println("Trying something new");
+        System.out.println("this is B1");
+	  System.out.println("Hi GitHub");
     }
 }
