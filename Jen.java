89a051732e69172fd2552172bcf680c0f179d96c
khldoon <218110351@psu.edu.sa>
2022-09-28 09:30:53 +0300

    Edited it for jenkins test Jen.java

--- Jen.java
+++ Jen.java
@@ -1,5 +1,6 @@
 public class Demo{
     public static void main(string[] args){
         System.out.println("JOB JOB JOB JOB!!!!");
+        System.out.println("Added this to test jenkins");
     }
 }
